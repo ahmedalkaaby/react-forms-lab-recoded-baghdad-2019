d373e09da6c26e0a67cccb752e45716553c44fe7
K3be <ahmed.13.alkaaby@gmail.com>
2020-01-08 17:40:18 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,9 +4,22 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:'',
+      password:''
+    };
   }
-
+  handleInputChange = (event) =>{
+     this.setState({
+       [event.target.name]:event.target.value
+     })
+   }
+ handleSubmit = (e) => {
+   e.preventDefault()
+   if(this.state.username !='' && this.state.password !=''){
+     this.props.handleLogin(this.state)
+   }
+ }
   render() {
     return (
       <form>
